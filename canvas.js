6c4bdf04df6d5bf20e033ee8cf29a1c3a3bdc69c
Robert Vulpe <nytr0gen.george@gmail.com>
2015-11-28 17:30:29 +0200

    sketched out the basics

--- canvas.js
+++ canvas.js
@@ -1,18 +1,91 @@
+const POLYGON_DRAWING  = 0;
+const POLYGON_DRAWN    = 1;
+const POLYGON_FINISHED = 2;
+
+var Polygon = function() {
+    this.points = [];
+    this.state = POLYGON_DRAWING;
+    this.ctx = canvas.getContext('2d');
+}
+
+Polygon.prototype.add = function(p) {
+    this.points.push(p);
+};
+
+Polygon.prototype.finishedDrawing = function() {
+    this.state = POLYGON_DRAWN;
+    this.draw();
+}
+
+Polygon.prototype.draw = function () {
+    // Clear
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw Points
+    this.points.forEach(function(p) {
+        p.draw();
+    });
+
+    // Draw lines betweens
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = 'black';
+
+    var firstPoint = this.points[0];
+    this.ctx.moveTo(firstPoint.x, firstPoint.y);
+    for (var i = 1; i < this.points.length; i++) {
+        this.ctx.lineTo(this.points[i].x, this.points[i].y);
+    }
+    if (this.state >= POLYGON_DRAWN) {
+        this.ctx.lineTo(firstPoint.x, firstPoint.y);
+    }
+    this.ctx.stroke();
+
+    if (this.state == POLYGON_DRAWING && this.points.length > 1) {
+        var lastPoint = this.points[this.points.length - 1];
+        this.ctx.setLineDash([5]);
+        this.ctx.beginPath();
+        this.ctx.strokeStyle = 'blue';
+        this.ctx.moveTo(firstPoint.x, firstPoint.y);
+        this.ctx.lineTo(lastPoint.x, lastPoint.y);
+        this.ctx.stroke();
+        this.ctx.setLineDash(0);
+    }
+
+};
+
+Polygon.prototype.drawWithMouse = function(mouse) {
+    if (this.points.length == 0 || this.state != POLYGON_DRAWING) {
+        return;
+    }
+
+    this.draw();
+
+    var lastPoint = this.points[this.points.length - 1];
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = 'red';
+    this.ctx.moveTo(lastPoint.x, lastPoint.y);
+    this.ctx.lineTo(mouse.x, mouse.y);
+    this.ctx.stroke();
+
+    mouse.draw();
+};
+
 var Point = function(x, y) {
     this.x = x;
     this.y = y;
+    this.ctx = canvas.getContext('2d');
 };
 
 Point.prototype.draw = function() {
-    context.beginPath();
-    context.arc(this.x, this.y, 3, 0, 2 * Math.PI);
-    context.fillStyle = 'black';
-    context.fill();
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, 3, 0, 2 * Math.PI);
+    this.ctx.fillStyle = 'black';
+    this.ctx.fill();
 }
 
 function writeMessage(canvas, message) {
     var context = canvas.getContext('2d');
-    // context.clearRect(0, 0, canvas.width,   canvas.height);
+    // context.clearRect(0, 0, canvas.width, canvas.height);
     context.font = '18pt Helvetica Neue';
     context.fillStyle = 'black';
     context.fillText(message, 10, 25);
@@ -25,14 +98,32 @@ function getMousePos(canvas, evt) {
 
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
-canvas.addEventListener('mousemove', function(evt) {
+var polygon = new Polygon();
+var canvasMouseMove = function(evt) {
     var pos = getMousePos(canvas, evt);
     var message = 'Mouse position: ' + pos.x + ', ' + pos.y;
     writeMessage(canvas, message);
-}, false);
 
-canvas.addEventListener('click', function(evt) {
-    var pos = getMousePos(canvas, evt);
-    pos.draw();
-    console.log(pos);
-}, false)
+    var mouse = getMousePos(canvas, evt);
+    polygon.drawWithMouse(mouse);
+};
+canvas.addEventListener('mousemove', canvasMouseMove, false);
+
+var canvasClick = function(evt) {
+    var mouse = getMousePos(canvas, evt);
+    polygon.add(mouse);
+    polygon.draw();
+
+    console.log(mouse);
+};
+canvas.addEventListener('click', canvasClick, false)
+
+document.onkeypress = function(e) {
+    e = e || window.event;
+    var charCode = (typeof e.which == 'number') ? e.which : e.keyCode;
+    if (String.fromCharCode(charCode) == ' ') {
+        canvas.removeEventListener('mousemove', canvasMouseMove);
+        canvas.removeEventListener('click', canvasClick);
+        polygon.finishedDrawing();
+    }
+};
